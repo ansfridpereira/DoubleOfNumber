d66b79b631063c52e5977ce129df306ac65cb474
ansfridpereira <89774958+ansfridpereira@users.noreply.github.com>
2021-09-17 20:03:00 +0530

    Double of a number

---
+++ src/DoubleOfNumber.java
@@ -0,0 +1,10 @@
+class DoubleOfNumber {
+    public static void main(String[] args)
+    {
+        int num = 50;
+        System.out.println("Num "+num);
+        num=2*num;
+        System.out.println("Value of number multiplied by 2 is "+num);
+
+    }
+}
